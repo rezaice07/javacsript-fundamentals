c35a081cd0e3300d9a997843b8b0b576d202ba8d
Rejwanul Reja <rezaice07@gmail.com>
2021-12-23 08:56:13 +0600

    sfffs

--- app.js
+++ app.js
@@ -26,19 +26,17 @@ console.log(typeof true)
 console.log(typeof 1123)
 console.log(typeof 123.89)
 
-//function
+//property on function
 const person = {
     firstName: "John",
     lastName: "Doe",
     id: 5566,
-    //fullName:this.firstName + " " + this.lastName,
     fullName2: () => {
-        return person.firstName + " " + person.lastName;
+        return this.person.firstName + " " + person.lastName;
     }
 };
-
-//console.log(person.fullName); 
-console.log(person.fullName2());
+ 
+//console.log(person.fullName2());
 
 const handler = {}
 
@@ -51,5 +49,58 @@ handler.lisghtOnOff = (lightOn) => {
         document.getElementById('imgLoader').src = "https://www.w3schools.com/js/pic_bulboff.gif"
 }
 
+//object declaration
+const x = new String();        // Declares x as a String object
+const y = new Number();        // Declares y as a Number object
+const z = new Boolean();       // Declares z as a Boolean object
 
+const username='Rejwanul Reja'
+
+console.log(username)
+
+// date function
+
+const dt=new Date();
+
+console.log(dt)
+
+
+const dtw= Date();
+
+console.log(dtw)
+
+//Escape 
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text)
+
+//string object
+var str = new String("John");
+console.log(`str`, str[0])
+
+let strTest = "Please visit Microsoft and Microsoft!";
+let newText = strTest.replace('Microsoft', "W3Schools");
+
+console.log(`newText`, newText)
+
+/*
+
+slice(start, end)
+substring(start, end)
+substr(start, length)
+
+*/
+
+let htxt = "HELLO WORLD";
+let char = htxt.charAt(0);
+let charCode = htxt.charCodeAt(0);
+
+//console.log(`char`, charCode)
+console.log(`htxt[0]`, htxt[0])
+
+
+//foreach
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach((object)=> console.log(`object`, object));
 
